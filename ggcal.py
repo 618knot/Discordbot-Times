87bdba1a16d2d8c06c95fe569a300f788d6086d5
618knot <knot618@gmail.com>
2022-08-21 22:53:08 +0900

    ggcal.py追加

---
+++ ggcal.py
@@ -0,0 +1,36 @@
+from __future__ import print_function
+import datetime
+import os.path
+from googleapiclient.discovery import build
+from google.auth import load_credentials_from_file
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+
+def calendar_info1():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    # Load credential file for service account
+    creds = load_credentials_from_file(
+        'cistlt-calendar.json', SCOPES
+    )[0]
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+
+    # NOTE: Set your calendar id
+    events_result = service.events().list(calendarId='cist.lt.club@gmail.com', timeMin=now,
+                                        maxResults=1, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        return -1
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        return start, event['summary']
+
